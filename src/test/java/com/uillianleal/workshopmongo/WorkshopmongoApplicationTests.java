059a053d166e8b9e63736c2121c53267e6e3da45
Uillianleal <willianleal52@hotmail.com>
2021-11-04 11:29:35 -0300

    Primeiro commit - Projeto criado

---
+++ src/test/java/com/uillianleal/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uillianleal.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
