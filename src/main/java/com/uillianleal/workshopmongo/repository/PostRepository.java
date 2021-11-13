d9e1c74ae486cfa1a91d8a062d4f157de64cecae
Uillianleal <willianleal52@hotmail.com>
2021-11-13 09:20:01 -0300

    Consulta simples com query methods

--- src/main/java/com/uillianleal/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/uillianleal/workshopmongo/repository/PostRepository.java
@@ -1,12 +1,15 @@
 package com.uillianleal.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.uillianleal.workshopmongo.domain.Post;
-import com.uillianleal.workshopmongo.domain.User;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
