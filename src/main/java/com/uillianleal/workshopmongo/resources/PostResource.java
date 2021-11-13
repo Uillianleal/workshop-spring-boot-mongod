e89c55f668bccdad0afca076499345f4e5a2bc18
Uillianleal <willianleal52@hotmail.com>
2021-11-13 08:33:37 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/uillianleal/workshopmongo/resources/PostResource.java
+++ src/main/java/com/uillianleal/workshopmongo/resources/PostResource.java
@@ -1,7 +1,5 @@
 package com.uillianleal.workshopmongo.resources;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,6 +24,5 @@ public class PostResource {
 		return ResponseEntity.ok().body(obj);
 	
 	}
-	
-   
+	  
 }
